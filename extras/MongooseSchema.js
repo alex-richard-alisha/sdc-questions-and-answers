c8d540830de00435f5321ed04dd9c7259902b389
adbarc92 <adbarclay92@gmail.com>
2021-08-10 09:53:28 -0600

    First commit

---
+++ extras/MongooseSchema.js
@@ -0,0 +1,30 @@
+
+const questionSchema = new Schema({
+	question_id: Number,
+	product_id: Number,
+	question_body: String,
+	question_date: String | Date,
+	question_helpfulness: Number,
+	reported: Boolean,
+	answers: {
+		answer_id: {
+			answer_id: Number,
+			answer_body: String,
+			answer_date: String | Date,
+			answerer_name: String,
+			helpfulness: Number,
+			photos: [{id: Number, url: String}]
+		}
+	}
+});
+
+const answerSchema = new Schema({
+	question_id: Number,
+	answer_id: Number,
+	answer_body: String,
+	answer_date: String | Date,
+	answerer_name: String,
+	helpfulness: Number,
+	photos: [{id: Number, url: String}]
+})
+
